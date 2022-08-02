2d3fb40e96f8cb6d79f25cd0f1abfb15c6784f8f
jvos <john.voskuyl@gmail.com>
2022-08-02 11:03:02 -0700

    listing model done without relationships

---
+++ app/models/listing.py
@@ -0,0 +1,30 @@
+from .db import db
+
+class Listing(db.Model):
+    __tablename__ = 'listings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id',  ondelete="CASCADE"), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(125), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    updated_at = db.Column(db.String(200), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'title': self.title,
+            'description': self.description,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'country': self.country,
+            'price': self.price,
+            'updated_at': self.updated_at
+        }
