fd33a6e69bc97adeaa892cf3c003793288163d10
jvos <john.voskuyl@gmail.com>
2022-08-02 16:02:53 -0700

    seeders finished for basic listings

--- app/seeds/listings.py
+++ app/seeds/listings.py
@@ -14,11 +14,18 @@ def seed_listings():
     listing3 = Listing(
         user_id=1, title='Tahoe Cabin', description="Great Tahoe get away. This is a Tahoe Cabin that people dream about.",
         address="1585 Vivian Ln", city="Incline Village", state="NV", country="USA", price=400.00, updated_at=datetime.now())
-
+    listing4 = Listing(
+        user_id=2, title='Tahoe Lake House', description="Stay at this wonderful Tahoe dream house, you will not be dissapointed. One of kind home and one of a kind location.",
+        address="140 Marla Ln", city="Glenbrook", state="NV", country="USA", price=500.00, updated_at=datetime.now())
+    listing5 = Listing(
+        user_id=2, title='South Lake Tahoe Home', description="Near Stateline and all the fun actitivies. Would be an incredible place to stay if you are looking for some fun night life! Also close to Heavenly.",
+        address="4042 Sunrise Ln", city="South Lake Tahoe", state="CA", country="USA", price=375.00, updated_at=datetime.now())
 
     db.session.add(listing1)
     db.session.add(listing2)
     db.session.add(listing3)
+    db.session.add(listing4)
+    db.session.add(listing5)
 
 
     db.session.commit()
