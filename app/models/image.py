3d0a31b4830a889ee6b0962082df8ce1ee112f05
jvos <john.voskuyl@gmail.com>
2022-08-02 11:15:15 -0700

    image model completed

---
+++ app/models/image.py
@@ -0,0 +1,16 @@
+from .db import db
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    listing_id = db.Column(db.Integer, db.ForeignKey(
+        'listings.id',  ondelete="CASCADE"), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.listing_id,
+            'url': self.url,
+        }
