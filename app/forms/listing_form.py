ea1dcf65a565e81a4c3c11e155994b1ffeda3e52
jvos <john.voskuyl@gmail.com>
2022-08-02 18:07:29 -0700

    about to start price field for listing form

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -5,32 +5,32 @@ from wtforms.validators import DataRequired, ValidationError
 def title_length(form, field):
   title = field.data
   if len(title) > 80:
-    raise ValidationError('Title must be less than 80 characters.')
+    raise ValidationError('Title must be 80 characters or less.')
 
 def description_length(form, field):
   description = field.data
   if len(description) > 200:
-    raise ValidationError('Description must be less than 200 characters.')
+    raise ValidationError('Description must be 200 characters or less.')
 
 def address_length(form, field):
   address = field.data
   if len(address) > 125:
-    raise ValidationError('Address must be less than 125 characters.')
+    raise ValidationError('Address must be 125 characters or less.')
 
 def city_length(form, field):
   city = field.data
   if len(city) > 115:
-    raise ValidationError('City must be less than 115 characters.')
+    raise ValidationError('City must be 115 characters or less.')
 
 def state_length(form, field):
   state = field.data
   if len(state) > 2:
-    raise ValidationError('State must be less than 3 characters.')
+    raise ValidationError('State must be 2 characters or less.')
 
-def state_length(form, field):
+def country_length(form, field):
   state = field.data
   if len(state) > 115:
-    raise ValidationError('State must be less than 115 characters.')
+    raise ValidationError('Country must be 115 characters or less.')
 
 
 
@@ -41,4 +41,4 @@ class ListingForm(FlaskForm):
   address = StringField('Address', validators=[DataRequired(), address_length])
   city = StringField('City', validators=[DataRequired(), city_length])
   state = StringField('State', validators=[DataRequired(), state_length])
-  country = StringField('Country', validators=[DataRequired()])
+  country = StringField('Country', validators=[DataRequired(), country_length])
