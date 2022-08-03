f00bb9afde37d70c6a30260c99b550088c1c28ee
jvos <john.voskuyl@gmail.com>
2022-08-03 13:50:53 -0700

    pictures showing up

--- react-app/src/components/listings/Elements/ListingCard.js
+++ react-app/src/components/listings/Elements/ListingCard.js
@@ -4,8 +4,10 @@ import "./listingCard.css"
 
 function ListingCard({ listing }) {
 
+
   return (
     <div className="card-container">
+      <img src={listing.images[0].url} alt={listing.title}/>
       <h1>{listing.title}</h1>
       <h3>{listing.city}</h3>
       <h3>{listing.state}</h3>
