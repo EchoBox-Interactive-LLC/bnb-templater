ca9db4499e23c1763ce6e15bda44853ff51b97ee
jvos <john.voskuyl@gmail.com>
2022-08-02 19:39:39 -0700

    all back end routes for listings done

--- app/api/listing_routes.py
+++ app/api/listing_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, request
 from flask_login import login_required
 from app.api.auth_routes import validation_errors_to_error_messages
 from app.models import db, Listing
+from app.forms.listing_form import ListingForm, UpdateListingForm
 
 listing_routes = Blueprint('listings', __name__)
 
@@ -20,32 +21,63 @@ def get_listing(id):
         return {"listing": listing.to_dict()}
 
 
-# @listing_routes.route('/', methods=["POST"])
-# @login_required
-# def create_listing():
-    # form = ListingForm()
-    # form['csrf_token'].data = request.cookies['csrf_token']
+@listing_routes.route('/', methods=["POST"])
+@login_required
+def create_listing():
+    form = ListingForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
-    # if form.validate_on_submit():
+    if form.validate_on_submit():
 
-    #     listing = Listing(
-    #         user_id=form.data['user_id'],
-    #         category=form.data['category'],
-    #         name=form.data['name'],
-    #         event_image_url=form.data['image'],
-    #         date=form.data['date'],
-    #         description=form.data['description'],
-    #         price=form.data['price'],
-    #         max_occupancy=form.data['occupancy'],
-    #         tickets_available=form.data['occupancy'],
-    #         street_address=form.data['street_address'],
-    #         city=form.data['city'],
-    #         state=form.data['state'],
-    #         zip_code=form.data['zipCode'],
-    #     )
+        listing = Listing(
+            user_id=form.data['user_id'],
+            title=form.data['title'],
+            description=form.data['description'],
+            address=form.data['address'],
+            city=form.data['city'],
+            state=form.data['state'],
+            country=form.data['country'],
+            price=form.data['price'],
+            updated_at=form.data['updated_at']
+        )
 
-    #     db.session.add(listing)
-    #     db.session.commit()
-    #     return listing.to_dict()
+        db.session.add(listing)
+        db.session.commit()
+        return listing.to_dict()
 
-    # return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@listing_routes.route("/<int:id>", methods=["PUT"])
+@login_required
+def update_listing(id):
+    form = UpdateListingForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        listing = Listing.query.get(id)
+        if Listing.user_id != form.data['user_id']:
+            return {'errors': "This is not your listing and therefore you are unathorized to edit it."}, 401
+
+        listing.user_id = form.data['user_id'],
+        listing.title=form.data['title'],
+        listing.description=form.data['description'],
+        listing.address=form.data['address'],
+        listing.city=form.data['city'],
+        listing.state=form.data['state'],
+        listing.country=form.data['country'],
+        listing.price=form.data['price'],
+        listing.updated_at=form.data['updated_at']
+
+        db.session.commit()
+        return listing.to_dict()
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@listing_routes.route('/:id', methods=["DELETE"])
+@login_required
+def delete_listing(id):
+    listing = Listing.query.filter(Listing.id == id)
+    listing.delete()
+    db.session.commit()
+    return {'message': 'Your listing has been deleted'}
