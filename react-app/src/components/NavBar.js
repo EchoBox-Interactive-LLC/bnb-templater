ab3f7510c1b9c5e1871580e975b734aa97c5b91c
jvos <john.voskuyl@gmail.com>
2022-08-03 15:03:22 -0700

    fixing up navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,57 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import LogoutButton from "./auth/LogoutButton";
+import { login } from "../store/session";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+
+  const demoLogIn = () => {
+    dispatch(login("demo@aa.io", "password"));
+  };
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+      {!user && (
+        <div>
+          <h3>CloneBnB</h3>
+          <p>
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </p>
+          <button onClick={demoLogIn}>
+          Demo User
+        </button>
+          <p>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Login
+            </NavLink>
+          </p>
+          <p>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </p>
+        </div>
+      )}
+    {user && (
+      <div>
+        <h3>CloneBnB</h3>
+        <p>
+          <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
+        </p>
+        <p>
           <LogoutButton />
-        </li>
-      </ul>
+        </p>
+      </div>
+    )}
     </nav>
   );
-}
+    };
 
 export default NavBar;
