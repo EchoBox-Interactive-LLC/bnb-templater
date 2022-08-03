584a9d9debf51d8604a506166bd45d3216180391
jvos <john.voskuyl@gmail.com>
2022-08-02 18:04:05 -0700

    almost gone with listing form

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -12,7 +12,33 @@ def description_length(form, field):
   if len(description) > 200:
     raise ValidationError('Description must be less than 200 characters.')
 
+def address_length(form, field):
+  address = field.data
+  if len(address) > 125:
+    raise ValidationError('Address must be less than 125 characters.')
+
+def city_length(form, field):
+  city = field.data
+  if len(city) > 115:
+    raise ValidationError('City must be less than 115 characters.')
+
+def state_length(form, field):
+  state = field.data
+  if len(state) > 2:
+    raise ValidationError('State must be less than 3 characters.')
+
+def state_length(form, field):
+  state = field.data
+  if len(state) > 115:
+    raise ValidationError('State must be less than 115 characters.')
+
+
+
 class ListingForm(FlaskForm):
   user_id = IntegerField('User_Id', validators=[DataRequired()])
   title = StringField('Title', validators=[DataRequired(), title_length])
-  description = StringField('Description', validators=[DataRequired()] )
+  description = StringField('Description', validators=[DataRequired(), description_length])
+  address = StringField('Address', validators=[DataRequired(), address_length])
+  city = StringField('City', validators=[DataRequired(), city_length])
+  state = StringField('State', validators=[DataRequired(), state_length])
+  country = StringField('Country', validators=[DataRequired()])
