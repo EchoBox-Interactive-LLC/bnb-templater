fd4fd62f35291957f3e1a9a3165d1af1f1182650
jvos <john.voskuyl@gmail.com>
2022-08-03 21:07:42 -0700

    update and delete buttons for a listing added

--- react-app/src/components/listings/ListingDetails.js
+++ react-app/src/components/listings/ListingDetails.js
@@ -1,21 +1,40 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { retrieveListings } from "../../store/listings";
 
-function ListingDetails () {
+function ListingDetails() {
   const dispatch = useDispatch();
-  const history = useHistory();
 
-  const { listingId } =useParams();
-  const listing = useSelector((state) => state.listings[listingId])
-  const user = useSelector((state) => state.session.user)
+  const { listingId } = useParams();
+  const listing = useSelector((state) => state.listings[listingId]);
+  const user = useSelector((state) => state.session.user);
+
+  const [showUpdateButton, setShowUpdateButton] = useState(false);
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
 
   useEffect(() => {
     dispatch(retrieveListings());
-  }, [dispatch, listingId, history]);
+  }, [dispatch]);
 
-  return(
+  useEffect(() => {
+    if (listing) {
+      if (user.id === listing.user_id) {
+        setShowUpdateButton(true);
+        setShowDeleteButton(true);
+      }
+    }
+  }, [user.id]);
+
+  const updateListing = () => {
+    return;
+  };
+
+  const deleteListing = () => {
+    return;
+  };
+
+  return (
     <main>
       <h1>Listing Details Page</h1>
       {listing && (
@@ -24,19 +43,24 @@ function ListingDetails () {
           <p>{listing.city}</p>
           <p>{listing.state}</p>
           <p>{listing.country}</p>
-          <img
-          src={listing.images[0].url}
-          alt={listing.title}
-        />
-        <p>{listing.description}</p>
-        <p>$<span>{listing.price}</span>night</p>
+          {listing.images[0] && (
+            <img src={listing.images[0].url} alt={listing.title} />
+          )}
+          <p>{listing.description}</p>
+          <p>
+            $<span>{listing.price}</span>night
+          </p>
         </div>
       )}
-      {!listing && (
-        <h1>This Listing Does Not Exist</h1>
+      {!listing && <h1>This Listing Does Not Exist</h1>}
+      {showUpdateButton && (
+        <button onClick={updateListing}>Update Listing</button>
+      )}
+      {showDeleteButton && (
+        <button onClick={deleteListing}>Delete Listing</button>
       )}
     </main>
-  )
-};
+  );
+}
 
 export default ListingDetails;
