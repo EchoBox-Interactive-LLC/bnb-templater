d08b0618901998cd9c458a920d1769ace1922520
jvos <john.voskuyl@gmail.com>
2022-08-04 20:14:56 -0700

    working on create form

--- react-app/src/components/reviews/elements/CreateReviewModal.js
+++ react-app/src/components/reviews/elements/CreateReviewModal.js
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { makeReview } from "../../../store/reviews"
 
+function CreateReviewModal({ setShowCreateReviewModal }) {
+  const dispatch = useDispatch();
 
+  const userId = useSelector((state) => state.session.user.id);
+  const updated_at = new Date().toDateString();
+  const { listingId } = useParams();
 
-
-function CreateReviewModal() {
-
+  const [review, setReview] = useState("")
+  const [rating, setRating] = useState("")
 
   return (
     <h1>Charlie Brown</h1>
