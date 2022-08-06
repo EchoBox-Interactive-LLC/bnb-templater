3e32971ed65e0b50b59ba83a77a07825cb82a658
jvos <john.voskuyl@gmail.com>
2022-08-05 21:43:36 -0700

    styling nav bar

--- react-app/src/components/navbar/NavBar.js
+++ react-app/src/components/navbar/NavBar.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import LogoutButton from "../auth/LogoutButton";
 import { login } from "../../store/session";
-import { ReactComponent as Hamburger } from "../../images/hamburger.svg"
-import "./navBar.css"
+import { ReactComponent as Hamburger } from "../../images/hamburger.svg";
+import "./navBar.css";
 
 const NavBar = () => {
   const dispatch = useDispatch();
@@ -16,20 +16,14 @@ const NavBar = () => {
 
   return (
     <nav>
+      {/* Logged OUT user menu */}
       {!user && (
         <div className="nav-bar">
           <p className="nav-item">
-          <NavLink to="/" exact={true} activeClassName="active">
-            CloneBnB
-          </NavLink>
-        </p>
-          <button className="nav-item" onClick={demoLogIn}>
-          Demo User
-        </button>
-        <div>
-          <Hamburger />
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQNvWDvQb_rCtRL-p_w329CtzHmfzfWP0FIw&usqp=CAU" alt="default avatar" />
-        </div>
+            <NavLink to="/" exact={true} activeClassName="active">
+              CloneBnB
+            </NavLink>
+          </p>
           <p className="nav-item">
             <NavLink to="/login" exact={true} activeClassName="active">
               Login
@@ -40,31 +34,43 @@ const NavBar = () => {
               Sign Up
             </NavLink>
           </p>
+          <button className="nav-item" id="demo-user" onClick={demoLogIn}>
+            Demo User
+          </button>
+          <div className="user-menu">
+            <Hamburger />
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQNvWDvQb_rCtRL-p_w329CtzHmfzfWP0FIw&usqp=CAU"
+              alt="default avatar"
+            />
+          </div>
         </div>
       )}
-    {user && (
-      <div className="nav-bar">
-        <p className="nav-item">
-          <NavLink to="/" exact={true} activeClassName="active">
-            CloneBnB
-          </NavLink>
-        </p>
-        <div>
-          <Hamburger />
-          {user && (<img src={user.avatar} alt="user avatar" />)}
+
+      {/* Logged IN user menu */}
+      {user && (
+        <div className="nav-bar">
+          <p className="nav-item">
+            <NavLink to="/" exact={true} activeClassName="active">
+              CloneBnB
+            </NavLink>
+          </p>
+          <div>
+            <Hamburger />
+            {user && <img src={user.avatar} alt="user avatar" />}
+          </div>
+          <p className="nav-item">
+            <NavLink to="/create" exact={true} activeClassName="active">
+              New Listing
+            </NavLink>
+          </p>
+          <p className="nav-item">
+            <LogoutButton />
+          </p>
         </div>
-        <p className="nav-item">
-          <NavLink to="/create" exact={true} activeClassName="active">
-            New Listing
-          </NavLink>
-        </p>
-        <p className="nav-item">
-          <LogoutButton />
-        </p>
-      </div>
-    )}
+      )}
     </nav>
   );
-    };
+};
 
 export default NavBar;
