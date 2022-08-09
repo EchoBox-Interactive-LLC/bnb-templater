486bb83ffc7a6733cab4aaed5d894c195c7446d9
Jvos <john.voskuyl@gmail.com>
2022-08-09 10:12:03 -0700

    booking card almost done

--- react-app/src/components/bookings/elements/BookingCard.js
+++ react-app/src/components/bookings/elements/BookingCard.js
@@ -9,14 +9,48 @@ function BookingCard({ listing, reviews, rating }) {
     reviewsContent = " reviews";
   }
 
+  // Service free calc, using 3% for now
+  let serviceFee;
+  if (listing) {
+    serviceFee = Math.round((listing.price * .03))
+  }
+
+  // Calc for total amount
+  let total
+  if (listing) {
+    total = serviceFee + 150 + listing.price
+  }
+
   return (
     <div className="booking-card-container">
-      <div id="night-price">
-        <span style={{ fontWeight: "bold" }}>${listing.price}</span> night
+      <div className="top-booking-section">
+        <div id="night-price">
+          <span style={{ fontWeight: "bold" }}>${listing.price}</span> night
+        </div>
+        <div id="rating">⭑ {rating}</div>
+        <div id="reviews-num">
+          <div>
+            {reviews.length} {reviewsContent}
+          </div>
+        </div>
       </div>
-      <div id="rating">⭑ {rating}</div>
-      <div id="reviews-num">
-        <div>{reviews.length} {reviewsContent}</div>
+      <div className="middle-booking-section">
+        <div className="fee">
+          <p>Cleaning Fee</p>
+        </div>
+        <div id="rating">$150</div>
+      </div>
+      <div className="middle-booking-section">
+        <div className="fee">
+          <p>Service Fee</p>
+        </div>
+        <div>${serviceFee}</div>
+      </div>
+      <div className="total-booking-section">
+        <div id="total-fee">
+          <p>Total before taxes</p>
+        </div>
+        <div>${total}</div>
       </div>
     </div>
   );
