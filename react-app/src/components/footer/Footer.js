05021f527f552a9d37631af1c37e82083e4fde26
Jvos <john.voskuyl@gmail.com>
2022-08-09 21:34:00 -0700

    added created by stuff

--- react-app/src/components/footer/Footer.js
+++ react-app/src/components/footer/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import LinkedIn from "../../images/linkedIn.png";
+import GitHub from "../../images/github.svg";
 import "./footer.css";
 
 function Footer() {
-
   // let footer = document.getElementsByClassName('footer');
   // footer.classList.add('sticky-footer');
 
@@ -10,13 +11,22 @@ function Footer() {
     <footer className="sticky-footer">
       <div className="footer-flex">
         <h4 className="footer-flex-left footer-item">© 2022 Urbnb, Inc.</h4>
+        <div className="about">
+          <h2 id="created-by">Created By John Voskuyl</h2>
+          <a href="https://www.linkedin.com/in/john-voskuyl-a2214083/">
+            <img id="github" src={GitHub} alt="GitHub Logo" />
+          </a>
+          <a href="https://github.com/jvos415">
+            <img id="linkedin" src={LinkedIn} alt="LinkedIn Logo" />
+          </a>
+        </div>
         <div className="footer-flex-right">
-          <h4 id="language">English(US)</h4>
-          <h4 className="footer-item">$  USD</h4>
+          <p id="language">English(US)</p>
+          <p className="footer-item">$ USD</p>
         </div>
       </div>
     </footer>
-  )
-};
+  );
+}
 
 export default Footer;
