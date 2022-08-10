996a7b1089e0f9148d32e939ea61290bb48798c4
Jvos <john.voskuyl@gmail.com>
2022-08-10 15:45:20 -0700

    sign up almost done modal

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
 const SignUpForm = ({ setShowSignUpModal }) => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
@@ -17,10 +17,10 @@ const SignUpForm = ({ setShowSignUpModal }) => {
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     } else {
-      setErrors(["Password: The passwords entered do not match"])
+      setErrors(["Password: The passwords entered do not match"]);
     }
   };
 
@@ -41,59 +41,66 @@ const SignUpForm = ({ setShowSignUpModal }) => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   const cancelSignUp = () => {
-    setShowSignUpModal(false)
-  }
+    setShowSignUpModal(false);
+  };
 
   return (
     <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+      <div className="modal-top">
+        <button className="modal-cancel" onClick={cancelSignUp} type="button">
+          X
+        </button>
+        <h3 className="modal-title">Sign up</h3>
       </div>
       <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
+        <input className="input-field"
+          type="text"
+          placeholder="Username"
+          name="username"
           onChange={updateUsername}
           value={username}
         ></input>
       </div>
       <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
+        <input className="input-field"
+          placeholder="Email"
+          type="text"
+          name="email"
           onChange={updateEmail}
           value={email}
         ></input>
       </div>
       <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
+        <input className="input-field"
+          placeholder="Password"
+          type="password"
+          name="password"
           onChange={updatePassword}
           value={password}
         ></input>
       </div>
       <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
+        <input className="input-field"
+          placeholder="Confirm Password"
+          type="password"
+          name="repeat_password"
           onChange={updateRepeatPassword}
           value={repeatPassword}
           required={true}
         ></input>
       </div>
-      <button onClick={cancelSignUp} type='button'>Cancel</button>
-      <button type='submit'>Sign Up</button>
+      <div className="error-container">
+        {errors.map((error, ind) => (
+          <div className="errors" key={ind}>{error}</div>
+        ))}
+      </div>
+      <div className="submit-flex">
+      <button className="submit-button" type="submit">Sign up</button>
+      </div>
     </form>
   );
 };
