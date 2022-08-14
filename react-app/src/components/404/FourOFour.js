316ab5fdf77bd008d5dfb4599acc6524d751ba4a
Jvos <john.voskuyl@gmail.com>
2022-08-13 22:27:46 -0700

    added a back to main page button on the 404 page

--- react-app/src/components/404/FourOFour.js
+++ react-app/src/components/404/FourOFour.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./fourOFour.css";
 
 function FourOFour() {
-    return (
-        <main>
-            <div className="page-not-found-container">
-                <h1 className="page-not-found-title">404: Page Not Found!</h1>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="page-not-found-container">
+        <h1 className="page-not-found-title">404: Page Not Found!</h1>
+        <NavLink to="/">
+          <h3 className="page-not-found-home-link">Back to exploring</h3>
+        </NavLink>
+      </div>
+    </main>
+  );
 }
 
 export default FourOFour;
