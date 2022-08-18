b801332f826dd328b5c7f437f59dac7e20d5cdc4
Jvos <john.voskuyl@gmail.com>
2022-08-17 19:38:13 -0700

    delete an image is working

--- react-app/src/components/image_things/elements/ImageCard.js
+++ react-app/src/components/image_things/elements/ImageCard.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { removeImage } from "../../../store/images";
 import "./imageCard.css";
 
 function ImageCard({ image, listing }) {
     const dispatch = useDispatch();
 
     const user = useSelector((state) => state.session.user)
+
+    const [showDeleteButton, setShowDeleteButton] = useState(false)
 
     useEffect(() => {
         if (user && listing) {
@@ -14,15 +17,17 @@ function ImageCard({ image, listing }) {
             }
         }
 
-    }, [user])
-
-    const [showDeleteButton, setShowDeleteButton] = useState(false)
+    }, [user, listing])
+    
+    const deleteImage = () => {
+        dispatch(removeImage(image.id))
+    }
     
     return (
     <div className="single-image-container">
       <img className="standard-image" src={image.url} alt="listing"></img>
       {showDeleteButton && (
-        <button className="delete-image-button">Delete Image</button>
+        <button onClick={deleteImage} className="delete-image-button">Delete Image</button>
       )}
     </div>
   );
