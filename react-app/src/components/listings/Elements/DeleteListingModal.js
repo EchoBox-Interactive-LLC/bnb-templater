01dcff2c8d1f5ce8e968e4b68d8f843dc8e07bf8
Jvos <john.voskuyl@gmail.com>
2022-08-17 21:22:42 -0700

    delete listing modal styled

--- react-app/src/components/listings/Elements/DeleteListingModal.js
+++ react-app/src/components/listings/Elements/DeleteListingModal.js
@@ -4,25 +4,39 @@ import { useHistory } from "react-router-dom";
 import { removeListing } from "../../../store/listings";
 
 function DeleteListingModal({ setShowDeleteModal, listingId }) {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const deleteListing = () => {
-        dispatch(removeListing(listingId));
-        history.push(`/`);
-      };
+  const deleteListing = () => {
+    dispatch(removeListing(listingId));
+    history.push(`/`);
+  };
 
-      const cancelDeleteListing = () => {
-        setShowDeleteModal(false)
-      };
+  const cancelDeleteListing = () => {
+    setShowDeleteModal(false);
+  };
 
-    return (
-        <div>
-            <h1>Are you sure you want to delete your listing?</h1>
-            <button onClick={cancelDeleteListing}>Cancel</button>
-            <button onClick={deleteListing}>Delete Listing</button>
-        </div>
-    )
+  return (
+    <div>
+      <div id="delete-listing-modal-top" className="modal-top">
+        <h3 id="delete-listing-title" className="modal-title">
+          Are you sure you want to delete your listing?
+        </h3>
+        <button
+          className="modal-cancel"
+          onClick={cancelDeleteListing}
+          type="button"
+        >
+          X
+        </button>
+      </div>
+      <div className="submit-flex">
+        <button id="delete-listing-button" className="submit-button" onClick={deleteListing}>
+          Delete Listing
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default DeleteListingModal;
