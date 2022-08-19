8bb9452041b90da99c7524edec27e059aed7d9bc
Jvos <john.voskuyl@gmail.com>
2022-08-18 21:01:49 -0700

    little styled back arrow added to all listings photo page

--- react-app/src/components/listings/ListingImages.js
+++ react-app/src/components/listings/ListingImages.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import ImageCard from "../image_things/elements/ImageCard";
 import { retrieveImage } from "../../store/images";
@@ -9,6 +9,7 @@ import "./listingImages.css";
 function ListingImages() {
   const { listingId } = useParams();
   const dispatch = useDispatch();
+  const history = useHistory();
 
 
   const images = Object.values(useSelector((state) => state.images)).filter(
@@ -23,8 +24,17 @@ function ListingImages() {
     dispatch(retrieveListings())
   }, [dispatch]);
 
+  const backToListing = () => {
+    history.push(`/listings/${listingId}`)
+  }
+
   return (
     <main>
+      <div>
+        <button className="back-arrow" onClick={backToListing} type="button">
+          <img id="back-arrow-img" src={`https://cdn-icons.flaticon.com/png/512/2722/premium/2722991.png?token=exp=1660882024~hmac=429688992dfe166d843b79540a533fbe`} alt="back arrow"/>
+        </button>
+      </div>
       <div className="all-images-container">
         {images &&
           images.map((image) => {
