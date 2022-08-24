06ce2f9edf8a417d581c2dbe21ba999a61237bd7
Jvos <john.voskuyl@gmail.com>
2022-08-23 21:24:42 -0700

    read trips functionality working

--- react-app/src/components/bookings/Trips.js
+++ react-app/src/components/bookings/Trips.js
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { retrieveBookings } from "../../store/bookings";
+import { retrieveListings } from "../../store/listings";
 import TripCard from "./elements/TripCard";
 import noTrips from "../../images/no_trips.webp";
 import wavingHand from "../../images/waving-hand.png";
-import "./trips.css"
+import "./trips.css";
 
 function Trips() {
   const dispatch = useDispatch();
@@ -24,9 +25,13 @@ function Trips() {
     dispatch(retrieveBookings());
   }, [dispatch, bookings.length]);
 
+  useEffect(() => {
+    dispatch(retrieveListings());
+  }, [dispatch]);
+
   const goHome = () => {
-    history.push("/")
-  }
+    history.push("/");
+  };
 
   return (
     <main>
@@ -34,29 +39,36 @@ function Trips() {
         <h1 id="trips-title">Trips</h1>
       </div>
 
-    {/* If there are bookings */}
-      <div>
-        {bookings.length > 0 &&
-          bookings.map((booking) => {
-            return <div key={booking.id}>
-              <TripCard booking={booking}/>
-            </div>;
-          })}
-      </div>
+      {/* If there are bookings */}
+      {bookings.length > 0 && (
+        <div>
+          <h2>Upcoming reservations</h2>
+        </div>
+      )}
+      {bookings.length > 0 &&
+        bookings.map((booking) => {
+          return (
+            <div key={booking.id}>
+              <TripCard booking={booking} />
+            </div>
+          );
+        })}
 
       {/* If there are no bookings */}
       {bookings.length === 0 && (
         <div className="no-trips-container">
           <div className="no-trips-left">
-            <img id="waving-hand" src={wavingHand} alt="waving hand"/>
+            <img id="waving-hand" src={wavingHand} alt="waving hand" />
             <h3>No trips booked... yet!</h3>
             <p>
               Time to dust off your bags and start planning your next adventure
             </p>
-            <button className="start-searching-button" onClick={goHome}>Start Searching</button>
+            <button className="start-searching-button" onClick={goHome}>
+              Start Searching
+            </button>
           </div>
           <div className="no-trips-right">
-            <img id="no-trips-img" src={noTrips} alt="family on deck"/>
+            <img id="no-trips-img" src={noTrips} alt="family on deck" />
           </div>
         </div>
       )}
--- react-app/src/components/bookings/elements/TripCard.js
+++ react-app/src/components/bookings/elements/TripCard.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-function TripCard() {
+function TripCard({ booking }) {
+  const listing = useSelector((state) => state.listings[booking.listing_id]);
 
-    return (
+  return (
+    <div>
+      {listing && (
         <div>
-            <h1>Something</h1>
+          <h1>{listing.city}</h1>
+          <h4>Entire home hosted by {listing.user[0].username}</h4>
         </div>
-    )
+      )}
+      {booking && (
+        <div>
+          <h3>{booking.start_date}</h3>
+          <h3>{booking.end_date}</h3>
+        </div>
+      )}
+      {listing && (
+        <div>
+          <h3>{listing.address}</h3>
+          <h3>{listing.city}, {listing.state}</h3>
+          <h3>{listing.country}</h3>
+        </div>
+      )}
+      {listing && (
+        <div>
+          <img src={listing.images[0].url}
+                          onError={(e) => {
+                            e.target.onerror = null;
+                            e.target.src =
+                              "https://images.unsplash.com/photo-1616555670626-09496d2eed9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJva2VuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
+                          }} alt={listing.title} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TripCard;
