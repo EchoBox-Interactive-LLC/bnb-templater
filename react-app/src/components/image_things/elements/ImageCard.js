92e8aa126d2496b7a8ddd1633afad07d98aec3b2
Jvos <john.voskuyl@gmail.com>
2022-08-23 20:31:59 -0700

    fixed image photo error on all photos page

--- react-app/src/components/image_things/elements/ImageCard.js
+++ react-app/src/components/image_things/elements/ImageCard.js
@@ -5,7 +5,6 @@ import DeleteImageModal from "./DeleteImageModal";
 import "./imageCard.css";
 
 function ImageCard({ image, listing }) {
-
   const user = useSelector((state) => state.session.user);
 
   const [showDeleteButton, setShowDeleteButton] = useState(false);
@@ -19,12 +18,23 @@ function ImageCard({ image, listing }) {
     }
   }, [user, listing]);
 
-
   return (
     <div className="single-image-container">
-      <img className="standard-image" src={image.url} alt="listing"></img>
+      <img
+        className="standard-image"
+        src={image.url}
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src =
+            "https://images.unsplash.com/photo-1616555670626-09496d2eed9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJva2VuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60";
+        }}
+        alt="listing"
+      ></img>
       {showDeleteButton && (
-        <button onClick={() => setShowDeleteImageModal(true)} className="delete-image-button">
+        <button
+          onClick={() => setShowDeleteImageModal(true)}
+          className="delete-image-button"
+        >
           Delete Image
         </button>
       )}
