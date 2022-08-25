2ccf4b642048967793c362e5a8d9414ee638f3f7
Jvos <john.voskuyl@gmail.com>
2022-08-24 18:32:23 -0700

    error messages fixed up in the back end

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, FloatField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 from datetime import datetime
 
@@ -7,13 +7,13 @@ from datetime import datetime
 def start_date_in_future(form, field):
     start_date = field.data
     if start_date < datetime.today().strftime("%Y-%m-%d"):
-        raise ValidationError('Start date cannot be in the past')
+        raise ValidationError('Check-in date cannot be in the past')
 
 
 def end_date_in_future(form, field):
     end_date = field.data
     if end_date < datetime.today().strftime("%Y-%m-%d"):
-        raise ValidationError('End date cannot be in the past')     
+        raise ValidationError('Checkout date cannot be in the past')     
 
 
 def min_guest_num(form, field):
@@ -31,8 +31,8 @@ def max_guest_num(form, field):
 class BookingForm(FlaskForm):
     listing_id = IntegerField('Listing_Id', validators=[DataRequired()])
     user_id = IntegerField('User_Id', validators=[DataRequired()])
-    start_date = StringField('Start_Date', validators=[DataRequired('Start date required'), start_date_in_future])
-    end_date = StringField('End_Date', validators=[DataRequired('End date required'), end_date_in_future])
+    start_date = StringField('Start_Date', validators=[DataRequired('Check-in date required'), start_date_in_future])
+    end_date = StringField('End_Date', validators=[DataRequired('Checkout date required'), end_date_in_future])
     guest_num = IntegerField('Guest_Num', validators=[DataRequired('Guest number is required'), min_guest_num, max_guest_num])
     updated_at = StringField('Update_At', validators=[DataRequired()])
 
@@ -40,9 +40,9 @@ class BookingForm(FlaskForm):
 class UpdateBookingForm(FlaskForm):
     listing_id = IntegerField('Listing_Id', validators=[DataRequired()])
     user_id = IntegerField('User_Id', validators=[DataRequired()])
-    start_date = StringField('Start_Date', validators=[DataRequired('Start date required'), start_date_in_future])
-    end_date = StringField('End_Date', validators=[DataRequired('End date required'), end_date_in_future])
-    guest_num = IntegerField('Guest_Num', validators=[DataRequired('Number of guests is required'), min_guest_num, max_guest_num])
+    start_date = StringField('Start_Date', validators=[DataRequired('Check-in date required'), start_date_in_future])
+    end_date = StringField('End_Date', validators=[DataRequired('Checkout date required'), end_date_in_future])
+    guest_num = IntegerField('Guest_Num', validators=[DataRequired('Guest number is required'), min_guest_num, max_guest_num])
     updated_at = StringField('Update_At', validators=[DataRequired()])
 
 
