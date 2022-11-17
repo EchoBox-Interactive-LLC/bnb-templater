e6e0ef708d350b262b4a1fc666bcc385e903f8f0
Jvos <john.voskuyl@gmail.com>
2022-11-17 11:56:59 -0800

    setting up flask schema completed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,7 @@ from .users import seed_users, undo_users
 from .listings import seed_listings, undo_listings
 from .images import seed_images, undo_images
 from .reviews import seed_reviews, undo_reviews
+from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,14 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.listings RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.images RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     seed_listings()
     seed_images()
